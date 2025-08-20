3da48e101487d490ed86a5318cd4571f19bc1d1b
quachanhbietbay <khanhlay0405@gmail.com>
2025-08-20 10:32:14 +0700

    update config  baseURL for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist",   // thư mục build (mặc định là dist, nhưng ghi rõ cho chắc)
+  },
+  base: "./",         // quan trọng để deploy, tránh lỗi đường dẫn trắng
 })
